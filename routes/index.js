dd2ba658c6883ca707b2483a9fadcd6568e1a3b4
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-04-01 13:33:20 +0100

    added everything disney said was a lie

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,17 @@ router.get('/the-burly-bearded-bartender',function (req, res,next) {
     });
 });
 
+router.get('/everything-disney-said-was-a-lie',function (req, res,next) {
+    var newurl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22Everything%20Disney%20Said%20Was%20A%20Lie%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
+    var q, parsedStory;
+    request(newurl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            q = JSON.parse(body);
+            q = q[0];
+            res.render('index', {title: q.title, story: q.story, link_to_code: q.code_adventure_url, link_to_answer: q.code_oracle_url });
+        }
+    });
+});
 
 module.exports = router;
 
