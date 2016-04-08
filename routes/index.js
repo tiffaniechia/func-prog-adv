cdc9065f0a7259973886de43202b76f3aa67e831
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-04-08 09:42:06 +0100

    added how does a sphinx have a boob job

--- routes/index.js
+++ routes/index.js
@@ -33,5 +33,17 @@ router.get('/everything-disney-said-was-a-lie',function (req, res,next) {
     });
 });
 
+router.get('/how-does-a-sphinx-have-a-boob-job',function (req, res,next) {
+    var newurl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22How%20Does%20A%20Sphinx%20Have%20A%20Boob%20Job%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
+    var q, parsedStory;
+    request(newurl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            q = JSON.parse(body);
+            q = q[0];
+            res.render('index', {title: q.title, story: q.story, link_to_code: q.code_adventure_url, link_to_answer: q.code_oracle_url });
+        }
+    });
+});
+
 module.exports = router;
 
