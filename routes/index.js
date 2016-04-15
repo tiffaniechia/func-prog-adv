1ff2468ba09d7c34d50e4883dc711c6525792bc0
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-04-15 09:41:27 +0100

    adds curryate kid

--- routes/index.js
+++ routes/index.js
@@ -45,5 +45,18 @@ router.get('/how-does-a-sphinx-have-a-boob-job',function (req, res,next) {
     });
 });
 
+router.get('/curryate-kid',function (req, res,next) {
+    var newurl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22Curryate%20kid%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
+    var q, parsedStory;
+    request(newurl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            q = JSON.parse(body);
+            q = q[0];
+            res.render('index', {title: q.title, story: q.story, link_to_code: q.code_adventure_url, link_to_answer: q.code_oracle_url });
+        }
+    });
+});
+
+
 module.exports = router;
 
