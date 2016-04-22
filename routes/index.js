bd46092866b4f9409baad0744eab571908cbf43c
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-04-22 13:51:29 +0100

    adds heroin bricks and rainbow coloured meth

--- routes/index.js
+++ routes/index.js
@@ -58,5 +58,18 @@ router.get('/curryate-kid',function (req, res,next) {
 });
 
 
+router.get('/heroin-bricks-and-rainbow-coloured-meth',function (req, res,next) {
+    var newurl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22Heroin%20Bricks%20and%20Rainbow%20Coloured%20Meth%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
+    var q, parsedStory;
+    request(newurl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            q = JSON.parse(body);
+            q = q[0];
+            res.render('index', {title: q.title, story: q.story, link_to_code: q.code_adventure_url, link_to_answer: q.code_oracle_url });
+        }
+    });
+});
+
+
 module.exports = router;
 
