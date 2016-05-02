43640fb0959c78c7a62ae277ec7a49141678432f
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-05-02 04:13:20 +0100

    fixed titles

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,7 @@ function logger(req, res, next) {
 }
 
 router.get('/adventures/:title',function (req, res,next) {
-    var title = req.params.title.replace(/(\-[a-z])/g, function($1){return $1.toUpperCase();});
-    var parsedTitle  = title.replace(/-/g,"%20");
+    var parsedTitle = req.params.title.replace(/-/g,"%20");
     var MongoDBUrl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22'+ parsedTitle +'%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
     var query;
     request(MongoDBUrl, function (error, response, body) {
