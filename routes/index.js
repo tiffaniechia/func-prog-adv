eadc6c756dd84a8dede4589ae35f149e8e24e812
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-05-02 04:01:27 +0100

    refactoring api

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,19 @@ function logger(req, res, next) {
     next();
 }
 
+router.get('/adventures/:title',function (req, res,next) {
+    var parsedTitle = req.params.title.replace(/-/g,"%20");
+    var MongoDBUrl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22'+ parsedTitle +'%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
+    var query;
+    request(MongoDBUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            query = JSON.parse(body);
+            query = query[0];
+            res.render('index', {title: query.title, story: query.story, link_to_code: query.code_adventure_url, link_to_answer: query.code_oracle_url });
+        }
+    });
+});
+
 router.get('/the-burly-bearded-bartender',function (req, res,next) {
     var newurl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22The%20Burly%20Bearded%20Bartender%22}&apiKey=_t6ifED-gRfhDc3c2IEtK3_nZ5tQ2K2F';
     var q, parsedStory;
