515773c58c91d489a58782818bf86c30ab9511d4
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-05-02 05:22:38 +0100

    adding root page

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,32 @@ function logger(req, res, next) {
     next();
 }
 
+var base_url='https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?';
+
 router.get('/adventures/:title',function (req, res,next) {
     var parsedTitle = req.params.title.replace(/-/g,"%20");
-    var MongoDBUrl = 'https://api.mlab.com/api/1/databases/functional-prog-adventure/collections/adventures?q={%22title%22:%20%22'+ parsedTitle +'%22}&apiKey=' + process.env.API_KEY;
+    var MongoDBUrl = base_url + 'q={%22title%22:%20%22'+ parsedTitle +'%22}&apiKey=' + process.env.API_KEY;
     var query;
     request(MongoDBUrl, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             query = JSON.parse(body);
             query = query[0];
-            res.render('index', {title: query.title, story: query.story, link_to_code: query.code_adventure_url, link_to_answer: query.code_oracle_url });
+            res.render('adventure', {title: query.title, story: query.story, link_to_code: query.code_adventure_url, link_to_answer: query.code_oracle_url });
         }
     });
 });
 
+router.get('/',function (req, res,next) {
+    var MongoDBUrl = base_url + '?&f={%22title%22:1}&apiKey=' + process.env.API_KEY;
+    var query;
+    request(MongoDBUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            query = JSON.parse(body);
+            query = query[0];
+            res.render('home', list: query);
+        }
+    });
+});
 
 module.exports = router;
 
